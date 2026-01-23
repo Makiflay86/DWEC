9cb0e0b978a2442839adf7c0c4c3f5b9af937841
Francisco Aybar <frannur1.ir@gmail.com>
2026-01-23 13:26:59 +0100

    Terminado

--- Tema5/05-11 Módulos CommonJS, AMD y ESM/ej7/main.js
+++ Tema5/05-11 Módulos CommonJS, AMD y ESM/ej7/main.js
@@ -8,3 +8,24 @@ reportArea(square1.length, reportList);
 reportPerimeter(square1.length, reportList);
 // Use the default
 let square2 = randomSquare(myCanvas.ctx);
+
+
+
+
+const picker = document.getElementById('picker');
+const btn = document.getElementById('btn');
+const contenedor = document.getElementById('contenedor');
+
+btn.addEventListener('click', () => {
+    const cuadrado = document.createElement('div');
+    const colorSeleccionado = picker.value;
+
+    cuadrado.style.width = '50px';
+    cuadrado.style.height = '50px';
+    cuadrado.style.backgroundColor = colorSeleccionado;
+    cuadrado.style.display = 'inline-block';
+    cuadrado.style.margin = '5px';
+    cuadrado.style.border = '1px solid #000';
+
+    contenedor.appendChild(cuadrado);
+});
