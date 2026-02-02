507836dae1019e4b299b544a795478d35864ff2b
Francisco Aybar <frannur1.ir@gmail.com>
2026-02-02 18:15:28 +0100

    Terminado

---
+++ Tema8/08-03 Node JS.3. Creación de un Servidor Web/nodeserver/servidorwebexpress/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public/'));
+app.listen('3500', function() {
+    console.log('Servidor web escuchando en el puerto 3500');
+});
